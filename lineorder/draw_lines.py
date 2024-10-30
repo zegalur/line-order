1a29cfd7fc1c79cd9d56e4fde02aa2de65658eb9
zegalur <zegalur@gmail.com>
2024-10-30 19:57:17 +0200

    better support of multiline cross-points

--- lineorder/draw_lines.py
+++ lineorder/draw_lines.py
@@ -85,10 +85,19 @@ def draw_lines(
                 is_non_overlapping = True
                 for u in range(len(input)):
                     if u in [i,j,k]: continue
-                    s1 = np.sign(get_line_eq_value(c[u], v1))
-                    s2 = np.sign(get_line_eq_value(c[u], v2))
-                    s3 = np.sign(get_line_eq_value(c[u], v3))
+                    h1 = get_line_eq_value(c[u], v1)
+                    h2 = get_line_eq_value(c[u], v2)
+                    h3 = get_line_eq_value(c[u], v3)
+                    s1 = np.sign(h1)
+                    s2 = np.sign(h2)
+                    s3 = np.sign(h3)
                     if (s1 != s2) or (s1 != s3) or (s2 != s3):
+                        # Check maybe one of the points is at the line,
+                        # while two others are at the same side.
+                        if (abs(h1) < epsilon) and (s2 == s3): continue
+                        if (abs(h2) < epsilon) and (s1 == s3): continue
+                        if (abs(h3) < epsilon) and (s1 == s2): continue
+                        # Two points are at the different sides.
                         is_non_overlapping = False
                         break
 
